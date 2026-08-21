b47951cbbb7e95854591062d786ab4436c66db1d
Niranjan <amuthanabishekniranjan@gmail.com>
2026-08-21 14:07:04 +0530

    Finall Deployment

--- LifeDrop/server/routes/requestRoutes.js
+++ LifeDrop/server/routes/requestRoutes.js
@@ -1,17 +1,3 @@
-// const express = require('express');
-// const {
-//   createRequest,
-//   getAllRequests,
-// } = require('../controllers/requestController');
-// const { protect } = require('../middleware/authMiddleware');
-
-// const router = express.Router();
-
-// router.post('/', protect, createRequest);
-// router.get('/', protect, getAllRequests);
-
-// module.exports = router;
-
 const express = require('express');
 const {
   createRequest,
